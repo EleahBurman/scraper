7d8a9582d3a1e2b778f9dadfad1f5d06b41bef8f
eleahburman <eleahjburman@gmail.com>
2023-09-04 18:45:31 -0400

    Print table heads from table from wiki

--- scraper.py
+++ scraper.py
@@ -5,3 +5,7 @@ import sqlite3
 
 browser = mechanicalsoup.StatefulBrowser()
 browser.open("https://en.wikipedia.org/wiki/Comparison_of_Linux_distributions")
+#extract table headers
+th = browser.page.find_all("th", attrs={"class": "table-rh"})
+distribution = [value.text.replace("\n", "") for value in th]
+print(distribution)
